0b615acd29633106d2193469d1393ea6b028e485
sngtlm <124654581+sngtlm@users.noreply.github.com>
2023-02-07 00:18:56 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 displayName = 'minecraftServer'
 compartmentId = 'ocid1.tenancy.oc1..aaaaaaaa4d5h26osxmonwduti7gcaqvwciwogizsxznyuw76qgwhaz5jwboa'
-availabilityDomain = "kLlY:PHX-AD-1"
+availabilityDomain = ['kLlY:PHX-AD-1', 'kLlY:PHX-AD-2', 'kLlY:PHX-AD-2'] 
 imageId = "ocid1.image.oc1.phx.aaaaaaaaxfzn7tzdjnxzhal4uhqt57rvz3ypndgrlrlpcgpgnslsq7coxkka"
 subnetId = 'ocid1.subnet.oc1.phx.aaaaaaaahsobsvekg2kgllfgxc7zqb6avbar4vbacggodrkewblq6ubet5ra'
 ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCngj8ZPlFq1lZJJxB9bQcSAIsjtZyamncB5v1VZ/DyVoce0SJvXVFNm1LN4a7UaLuvzzTuNnAXeu1jYsaFQuKHLKHexfoVG9V6pyw2gitOjGmOgvIdt0aWPLxRA2DRdrZ3Mpw7ioCsQ0gMmB5tFDOe2a7/Iw1Rapwrk8n5wDmqeOxaE9TX6/ter5IW+GQJDjx6YJwGFvAoU4cqACdkXRiU4qTAkcfkB8cwNiivd5RzXXS8faExGqCdCTHy1MtkyzD/wAhsWYO0Dnhx+FcieuJH6dLHXA3cFcYoW5Z1MXFxZ5xDDdse22tgDGt/l8Mx0Dmjel7NLNcozz7k/yoQ3iXT ssh-key-2023-02-07"
@@ -13,6 +13,7 @@ import logging
 import time
 import sys
 import requests
+import random
 
 LOG_FORMAT = '[%(levelname)s] %(asctime)s - %(message)s'
 logging.basicConfig(
@@ -90,7 +91,7 @@ instance_detail = oci.core.models.LaunchInstanceDetails(
     metadata={
         "ssh_authorized_keys": ssh_authorized_keys
     },
-    availability_domain=availabilityDomain,
+    availability_domain=(random.choice(availabilityDomain)),
     shape='VM.Standard.A1.Flex',
     compartment_id=compartmentId,
     display_name=displayName,
